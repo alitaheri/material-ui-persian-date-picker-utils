876a87c38335b2305453ba53ffe0d5bca386ead8
Ali Taheri Moghaddar <ali.taheri.m@gmail.com>
2017-04-10 17:01:15 +0430

    Implement the utils

---
+++ README.md
@@ -0,0 +1,25 @@
+# Persian calendar utils for Material UI DatePicker
+
+This library provides implementation specific utility functions that can
+be used by the material-ui's DatePicker component to correctly render
+a Persian calendar.
+
+Please refer to material-ui's
+[documentation page for DatePicker](http://localhost:3000/#/components/date-picker)
+under localized example to learn how to use it.
+
+
+## Installation
+
+You can install this package with the following command:
+
+```sh
+npm install material-ui-persian-date-picker-utils
+```
+
+## Thanks
+
+Great thanks to [material-ui](https://github.com/callemall/material-ui) for their great react components.
+
+## License
+This project is licensed under the [MIT license](https://github.com/alitaheri/react-mixout/blob/master/LICENSE).
---
+++ index.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var moment = require('moment-jalaali');
+
+function addWeek(weekArray, week) {
+  var emptyDays = 7 - week.length;
+
+  for (var i = 0; i < emptyDays; ++i) {
+    week[weekArray.length ? 'push' : 'unshift'](null);
+  }
+
+  weekArray.push(week);
+};
+
+function getWeekArray(d, firstDayOfWeek) {
+  var daysInMonth = moment.jDaysInMonth(moment(d).jYear(), moment(d).jMonth());
+
+  var dayArray = [];
+  for (var i = 1; i <= daysInMonth; i++) {
+    dayArray.push(moment(d).jDate(i).toDate());
+  }
+
+  var weekArray = [];
+  var week = [];
+
+  dayArray.forEach(function (day) {
+    if (week.length > 0 && day.getDay() === firstDayOfWeek) {
+      addWeek(weekArray, week);
+      week = [];
+    }
+
+    week.push(day);
+
+    if (dayArray.indexOf(day) === dayArray.length - 1) {
+      addWeek(weekArray, week);
+    }
+  });
+
+  return weekArray;
+}
+
+module.exports = {
+  getWeekArray: getWeekArray,
+  getYear: function (date) {
+    return moment(date).jYear();
+  },
+  setYear: function (date, year) {
+    return moment(date).jYear(year).toDate();
+  },
+  addDays: function (date, days) {
+    return moment(date).add(days, 'days').toDate();
+  },
+  addMonths: function (date, months) {
+    return moment(date).add(months, 'jMonth').toDate();
+  },
+  addYears: function (date, years) {
+    return moment(date).add(years, 'jYear').toDate();
+  },
+  getFirstDayOfMonth: function (date) {
+    return moment(date).jDate(1).toDate();
+  },
+  monthDiff: function (date1, date2) {
+    var months = (moment(date1).jYear() - moment(date2).jYear()) * 12;
+    months += moment(date1).jMonth();
+    months -= moment(date2).jMonth();
+    return months;
+  }
+};
